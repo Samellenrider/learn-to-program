08a8585a67fc5e5706e3094edefd0f9a102d7531
Sam Ellenrieder <sam.ell@web.de>
2017-07-12 16:11:20 +0100

    chapter04

---
+++ chapter04/favenum.rb
@@ -0,0 +1,6 @@
+puts 'Hey mate, what\'s your favorite number?'
+favenum = gets.chomp
+favenumnew = favenum.to_i + 1
+
+puts 'I\'m sorry but wouldn\'t ' + favenumnew.to_s + ' be a much better favorite number? As you know bigger and better is always better!'
+
