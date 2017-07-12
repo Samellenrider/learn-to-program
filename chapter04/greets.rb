9dbe9f57ae3a618cdb5b71110c383c70f5a916ce
Sam Ellenrieder <sam.ell@web.de>
2017-07-12 15:49:39 +0100

    chapter04

---
+++ chapter04/greets.rb
@@ -0,0 +1,10 @@
+puts 'Hey, nice to meet you! What\'s your  first name?'
+firstname = gets.chomp
+
+puts 'Sweet, what\'s your second name?'
+secondname = gets.chomp
+
+puts 'Perfect! And finally, what\'s your last name?'
+lastname = gets.chomp
+
+puts 'It\'s a pleasure to meet you, ' + firstname + ' ' + secondname + ' ' + lastname + '!'
