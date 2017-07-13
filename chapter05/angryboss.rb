3affe0819d7c2a43890387e698a97f7f6395a44a
Sam Ellenrieder <sam.ell@web.de>
2017-07-13 11:33:59 +0100

    learn-to-program

---
+++ chapter05/angryboss.rb
@@ -0,0 +1,10 @@
+puts 'Greet your boss!'
+greet = gets.chomp
+puts greet
+
+puts 'WHAT THE FUCK DO YOU WANT FROM ME???? '
+puts ' '
+puts 'Ask him whatever you want.'
+answer = gets.chomp
+
+puts 'WHAT THE FUCK DOES THAT MEAN ' + '"' + answer + '"' + '?!?!?!? GET THE HELL OUTTA HERE!!! YOU\'RE FIRED!!!'
--- chapter05/fancystrings.rb
+++ chapter05/fancystrings.rb
@@ -11,7 +11,7 @@ puts var2.reverse
 puts var3.reverse
 puts ''
 
-letterstring = 'JHLubizGHGJndTIbsswO'
+letterstring = 'JHLubizGHGJndTIbsswO
 
 puts letterstring
 puts letterstring.upcase
@@ -32,12 +32,69 @@ puts''
 
 
 
-lineWidth = 50
+lineWidth = 25
 string = 'Hello beautiful human'
 
 puts string.ljust lineWidth
 puts string.center lineWidth
 puts string.rjust lineWidth
-puts string.ljust(lineWidth/3) + ' ' + string.center(lineWidth/3) + ' ' + string.rjust(lineWidth/3)
+puts string.ljust(lineWidth/3) + string.center(lineWidth/3) + string.rjust(lineWidth/3)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var1 = 'Fancy string methods are soo fancy.'
+var2 = 'Surfing is definitly the best sport in the world, no discussion about that!'
+var3 = '1 2 3 4 5 6 7 8 9'
+
+puts var1
+puts var2
+puts var3
+puts ''
+puts var1.reverse
+puts var2.reverse
+puts var3.reverse
+puts ''
+
+letterstring = 'JHLubizGHGJndTIbsswO
+
+puts letterstring
+puts letterstring.upcase
+puts letterstring.downcase
+puts letterstring.swapcase
+puts letterstring.capitalize
+puts ''
+
+lineWidth = 100
+puts(          'Life is a pleasure'.center(lineWidth))
+puts(      'It gives you wings to fly with'.center(lineWidth))
+puts(                 'Don\'t be Icarus'.center(lineWidth))
+puts''
+puts(  'I\'m just kidding son'.center(lineWidth))
+puts(                             'Fly as high as you like to'.center(lineWidth))
+puts('Just be patient on the way'.center(lineWidth))
+puts''
+
+
+
+lineWidth = 25
+string = 'Hello beautiful human'
+
+puts string.ljust lineWidth
+puts string.center lineWidth
+puts string.rjust lineWidth
+puts string.ljust(lineWidth/3) + string.center(lineWidth/3) + string.rjust(lineWidth/3)
