36a4103da05c34faffb4cae566b4a8bc50896ecf
Sam Ellenrieder <sam.ell@web.de>
2017-07-13 12:02:17 +0100

    learn-to-program

---
+++ chapter05/playljustrjust.rb
@@ -0,0 +1,10 @@
+lineWidth = 100
+puts " "
+puts "Chapter 1: Surfings history".ljust(lineWidth/2) + "page 1-43".rjust(lineWidth/2)
+puts " "
+puts "Chapter 2: Surfboard shapes".ljust(lineWidth/2) + "page 44-69".rjust(lineWidth/2)
+puts " "
+puts "Chapter 3: Surf ikons".ljust(lineWidth/2) + "page 70-98".rjust(lineWidth/2)
+puts " "
+puts "Chapter 4: Surf spots".ljust(lineWidth/2) + "page 99-120".rjust(lineWidth/2)
+puts " "
