a89d22cf481528f1cafee6d23e87a5d02d64fb12
Sam Ellenrieder <sam.ell@web.de>
2017-07-13 12:49:20 +0100

    learn-to-program

---
+++ chapter05/highermath.rb
@@ -0,0 +1,34 @@
+puts 'More Arithmetic'
+puts ' '
+puts 19**3
+puts 5**0.5
+puts 9/6
+puts 9%6
+puts((19-5).abs)
+puts((5-19).abs)
+puts ' '
+puts ' '
+puts 'Random Numbers'
+puts ' '
+puts rand
+puts rand
+puts rand
+puts (rand(101))
+puts (rand(501))
+puts (rand(1001))
+puts (rand(1))
+puts (rand(1))
+puts (rand(1))
+puts ' '
+puts 'There is a ' + rand(101).to_s + '% chance that the pointbreaks are firing this weekend!'
+puts 'But t be sure check them bevore you go. It could change every minute!'
+puts ' '
+puts ' '
+puts 'The Math Object'
+puts ' '
+puts(Math::PI)
+puts(Math::E)
+puts(Math.cos(Math::PI/3))
+puts(Math.tan(Math::PI/9))
+puts(Math.log(Math::E**6))
+puts((3  + Math.sqrt(9))/5)
